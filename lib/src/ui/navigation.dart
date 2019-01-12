330c91448149eefe75ea5cd7d06c454bea442436
salby <srdlarsen@gmail.com>
2019-01-12 12:07:57 +0100

    Added navigation drawer.

--- lib/src/ui/navigation.dart
+++ lib/src/ui/navigation.dart
@@ -6,6 +6,80 @@ class NavigationDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
 
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+
+            // Header with logo and close button.
+            DrawerHeader(),
+
+            // Drawer navigatio  items.
+            DrawerItem(title: 'Front page', route: '/'),
+            DrawerItem(title: 'Business', route: '/business'),
+            DrawerItem(title: 'Entertainment', route: '/entertainment'),
+            DrawerItem(title: 'Health', route: '/health'),
+            DrawerItem(title: 'Science', route: '/science'),
+            DrawerItem(title: 'Sports', route: '/sports'),
+            DrawerItem(title: 'Technology', route: '/technology'),
+            
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+class DrawerHeader extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.only(bottom: 20.0, left: 16.0),
+      title: Text('Novum', style: Theme.of(context).textTheme.display1),
+      leading: IconButton(
+        color: Colors.black87,
+        icon: Icon(Icons.close, semanticLabel: 'Close'),
+        onPressed: () => Navigator.pop(context),
+        tooltip: 'Close',
+      ),
+    );
+  }
+
+}
+
+class DrawerItem extends StatelessWidget {
+
+  DrawerItem({
+    @required this.title,
+    @required this.route,
+  }) : assert(title != null),
+       assert(route != null);
+
+  final String title;
+  final String route;
+
+  @override
+  Widget build(BuildContext context) {
+    Route currentRoute;
+    Navigator.popUntil(context, (route) {
+      currentRoute = route;
+      return true;
+    });
+    return ListTile(
+      enabled: currentRoute.settings.name != route,
+      contentPadding: EdgeInsets.only(left: 34.0),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.subtitle.copyWith(
+          color: currentRoute.settings.name == route
+            ? Colors.black54
+            : Colors.black87,
+          fontSize: 16.0,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      onTap: () => Navigator.of(context).pushNamed(route),
     );
   }
 
