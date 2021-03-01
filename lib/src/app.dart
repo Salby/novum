d24189958771269d032dc5bf74b235aebce913dc
34001956 <apascualagut@gmail.com>
2021-03-01 01:37:11 +0100

    Translation to Spanish

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -9,14 +9,13 @@ import 'package:newsapi_client/newsapi_client.dart';
 
 final SystemUiOverlayStyle uiStyle = SystemUiOverlayStyle(
   statusBarIconBrightness: Brightness.dark,
-  statusBarColor: Colors.grey[100],
+  statusBarColor: Colors.white,
   systemNavigationBarColor: Colors.white,
   systemNavigationBarIconBrightness: Brightness.dark,
-  systemNavigationBarDividerColor: Colors.black26,
+  systemNavigationBarDividerColor: Colors.white,
 );
 
 class NovumApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(uiStyle);
@@ -24,20 +23,18 @@ class NovumApp extends StatelessWidget {
       navigatorObservers: [VillainTransitionObserver()],
       debugShowCheckedModeBanner: false,
       theme: kNovumTheme,
-      title: 'Novum',
-      home: Browse(title: 'Front page'),
+      title: 'News App',
+      home: Browse(title: 'Titulares'),
       routes: {
-        '/home': (context) => Browse(title: 'Front page'),
+        '/home': (context) => Browse(title: 'Titulares'),
         '/search': (context) => Search(),
-        '/business': (context) => Browse(title: 'Business', category: Categories.business),
-        '/entertainment': (context) => Browse(title: 'Entertainment', category: Categories.entertainment),
-        '/health': (context) => Browse(title: 'Health', category: Categories.health),
-        '/science': (context) => Browse(title: 'Science', category: Categories.business),
-        '/sports': (context) => Browse(title: 'Sports', category: Categories.sports),
-        '/technology': (context) => Browse(title: 'Technology', category: Categories.technology),
+        '/business': (context) => Browse(title: 'Negocios', category: Categories.business),
+        '/entertainment': (context) => Browse(title: 'Entretenimiento', category: Categories.entertainment),
+        '/health': (context) => Browse(title: 'Salud', category: Categories.health),
+        '/science': (context) => Browse(title: 'Ciencia', category: Categories.business),
+        '/sports': (context) => Browse(title: 'Deportes', category: Categories.sports),
+        '/technology': (context) => Browse(title: 'Tecnología', category: Categories.technology),
       },
     );
-
   }
-
 }
